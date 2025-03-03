f583483c8eed801e4d3b3f527f8d70ae56a73410
Buddafinga4361 <167499070+Buddafinga4361@users.noreply.github.com>
2025-03-03 10:01:15 -0600

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,14 @@
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Please register or check your email and/or password.', 'danger')
+            return redirect(url_for('register'))
+    return render_template('login.html')
